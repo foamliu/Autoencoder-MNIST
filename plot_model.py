0a63d3d328c1576cd4045177e072717614c48bd7
Yang Liu <foamliu@stcgpu-13.corp.microsoft.com>
2018-05-14 12:38:03 +0800

    diagram works

--- plot_model.py
+++ plot_model.py
@@ -1,15 +1,14 @@
 import keras.backend as K
-from keras.layers import Input, Conv2D, UpSampling2D, MaxPooling2D, Reshape, Concatenate, Lambda, Multiply, Permute, \
-    Activation
+from keras.layers import Input, Conv2D, UpSampling2D, MaxPooling2D, Reshape, Concatenate, Lambda, Multiply, Permute, Activation, Dense, Flatten
 from keras.models import Model
 from keras.utils import plot_model
 
 num_channels = 3
 img_h = img_w = 224
 n_labels = 10
-img_input = Input(shape=(num_channels, img_h, img_w))
+img_input = Input(shape=(img_h, img_w, num_channels))
 
-x = Conv2D(512, (3, 3), padding="same")(img_input)
+x = Conv2D(64, (3, 3), padding="same")(img_input)
 
 orig = x  # Save output x
 x = MaxPooling2D()(x)
@@ -18,8 +17,8 @@ x = UpSampling2D()(x)
 
 # here we're going to reshape the data for a concatenation:
 # xReshaped and origReshaped are now split branches
-xReshaped = Reshape((1, 512, 224, 224))(x)
-origReshaped = Reshape((1, 512, 224, 224))(orig)
+xReshaped = Reshape((1, 224, 224, 64))(x)
+origReshaped = Reshape((1, 224, 224, 64))(orig)
 
 # concatenation - here, you unite both branches again
 # normally you don't need to reshape or use the axis var,
@@ -27,14 +26,14 @@ origReshaped = Reshape((1, 512, 224, 224))(orig)
 together = Concatenate(axis=1)([origReshaped, xReshaped])
 
 bool_mask = Lambda(lambda t: K.greater_equal(t[:, 0], t[:, 1]),
-                   output_shape=(512, 224, 224))(together)
+                   output_shape=(224, 224, 64))(together)
 mask = Lambda(lambda t: K.cast(t, dtype='float32'))(bool_mask)
 
 x = Multiply()([mask, x])
-x = Conv2D(512, (3, 3), padding="same")(x)
+x = Conv2D(64, (3, 3), padding="same")(x)
 
-x = Reshape((n_labels, img_h * img_w))(x)
-x = Permute((2, 1))(x)
+x = Flatten()(x)
+x = Dense(n_labels, input_shape=(img_h, img_w))(x)
 main_output = Activation('softmax')(x)
 
 model = Model(inputs=img_input, outputs=main_output)
