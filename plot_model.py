d97f733ecefef595e764513b4a2e990198186de5
Yang Liu <foamliu@yeah.net>
2018-05-14 11:09:57 +0800

    plot unpolling impl.

--- plot_model.py
+++ plot_model.py
@@ -1,13 +1,43 @@
-import keras
+import keras.backend as K
+from keras.layers import Input, Conv2D, UpSampling2D, MaxPooling2D, Reshape, Concatenate, Lambda, Multiply, Permute, \
+    Activation
+from keras.models import Model
 from keras.utils import plot_model
 
-input1 = keras.layers.Input(shape=(16,))
-x1 = keras.layers.Dense(8, activation='relu')(input1)
-input2 = keras.layers.Input(shape=(32,))
-x2 = keras.layers.Dense(8, activation='relu')(input2)
-added = keras.layers.Add()([x1, x2])  # equivalent to added = keras.layers.add([x1, x2])
+num_channels = 3
+img_h = img_w = 224
+n_labels = 10
+img_input = Input(shape=(num_channels, img_h, img_w))
 
-out = keras.layers.Dense(4)(added)
-model = keras.models.Model(inputs=[input1, input2], outputs=out)
+x = Conv2D(img_input, (3, 3), 512)
+
+orig = x  # Save output x
+x = MaxPooling2D()(x)
+
+x = UpSampling2D()(x)
+
+# here we're going to reshape the data for a concatenation:
+# xReshaped and origReshaped are now split branches
+xReshaped = Reshape((1, 512, 224, 224))(x)
+origReshaped = Reshape((1, 512, 224, 224))(orig)
+
+# concatenation - here, you unite both branches again
+# normally you don't need to reshape or use the axis var,
+# but here we want to keep track of what was x and what was orig.
+together = Concatenate(axis=1)([origReshaped, xReshaped])
+
+bool_mask = Lambda(lambda t: K.greater_equal(t[:, 0], t[:, 1]),
+                   output_shape=(512, 224, 224))(together)
+mask = Lambda(lambda t: K.cast(t, dtype='float32'))(bool_mask)
+
+x = Multiply()([mask, x])
+x = Conv2D(x, (3, 3), 512)
+
+x = Reshape((n_labels, img_h * img_w))(x)
+x = Permute((2, 1))(x)
+main_output = Activation('softmax')(x)
+
+model = Model(inputs=img_input, outputs=main_output)
+
 print(model.summary())
 plot_model(model, to_file='model.svg', show_layer_names=True, show_shapes=True)
